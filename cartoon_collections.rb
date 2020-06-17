45cb91787126f30f3e24e5ca4f93ca028331eee9
Haley Ferro <haleyvferro@gmail.com>
2020-06-17 01:14:23 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -10,7 +10,7 @@ planeteer_calls.map {|call| call.capitalize + "!"}
 end
 
 def long_planeteer_calls(planeteer_calls)
-  
+   planeteer_calls.any? {|call| call.length > 4}
   
   # Use an Enumerable to check if any string in the passed in array is greater than 4 characters long
   # Return the boolean result of this check
